87e381add0d4fe99c861945eba709802a99d89c5
TVALAN83 <t.valancauskas@gmail.comm>
2022-12-07 18:29:47 +0200

    js_nd_05.12.2022

---
+++ nd_script.js
@@ -0,0 +1,119 @@
+console.log ('hello world!!!');
+
+
+// 05.12.2022
+
+
+
+// 0.
+
+let myAge;
+myAge = 18;
+console.log(myAge);
+
+// 1.
+
+let user = 'Petras'; let age = 36;
+console.log(user, age);
+
+// 2.
+
+let my_age = 28;
+console.log (my_age);
+
+// 3.
+
+let setp1 = 'Pirmas žingsnis';
+console.log (setp1);
+
+//4. (klausimas)
+
+{const block_Size = 14 + 25;
+    console.log (block_Size);
+}
+
+
+
+//.5 (klausimas)
+
+const block_width = 100;
+console.log (block_width);
+
+
+//6. (klausimas)
+
+{let userAge = 40;
+let userInfo = userAge;
+console.log (userInfo);
+}
+
+//7. INFINITY, o ne NaN
+let userHeight = 180 / 0 ;
+console.log (userHeight);
+
+//.8 undefined ,o ne Null
+
+let userName;
+console.log (typeof userName);
+
+//9. (klausimas) nuėmiau kabutes
+
+let userSize = 45 / 8;
+console.log (typeof userSize);
+
+//1.1 a)
+
+let my_Age = 39; let height_me = 188; let foot_size = 44;
+
+console.log (my_Age, height_me, foot_size);
+//1.2 a)
+
+const _name = 'Tomas'; const _surname = 'Valancauskas'; let proffesion = "Programmer";
+
+console.log (_name, _surname, proffesion);
+
+//2.1 a)
+
+let vienas = 1;
+let du = 2;
+let trys = 3;
+console.log(vienas + du + trys);
+
+
+//2.2 a)
+
+const vardas = 'vardenis'; const pavarde = 'pavardenis'; let profesija = 'profesija yra programuotojas';
+console.log (vardas + ' '+ pavarde + ' ' + profesija);
+
+//3 a)
+
+const face = {eyeColor :"blue"};
+console.log (face.eyeColor);
+
+//3 b)
+
+const person_info = {
+    _user:"Tomas", user_Name:"Nickname"
+}
+console.log (person_info._user, person_info.user_Name);
+
+//3 c)
+
+const user_Name = "John";
+console.log(user_Name);
+
+//3 d)
+
+console.log (person_info._user = user_Name);
+
+//3 e)
+
+console.log('hello, '+ user_Name);
+
+
+//06.12.2022
+
+
+
+
+
