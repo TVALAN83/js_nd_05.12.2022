65fb316a3fa82c338be37a52198bf7acb29be8c0
TVALAN83 <t.valancauskas@gmail.comm>
2022-12-11 19:04:16 +0200

    trečiadienis_nd_11_12_2022

--- nd_script.js
+++ nd_script.js
@@ -244,15 +244,15 @@ console.log(plotas(20,50));
 
 console.log('-----------------------------------------');
 
-//5 UŽDAVINYS - parašykite funkciją, kuriai būtų paduodami 2 parametrai, skaičiai, ir funkcija grąžintų TRUE jei dviejų skaičių suma yra mažiau arba lygu 100 ir false jei daugiau už 100.
+//5 UŽDAVINYS - parašykite funkciją, kuriai būtų paduodami 2 parametrai, skaičiai, ir funkcija grąžintų TRUE jei dviejų skaičių suma yra mažiau arba lygu 100 ir false jei daugiau už 100. Prie [else] sąlyga nebesirašo kaip ši (sk3 > 100), nebent [else if].
 
 function tikrinimas (sk_1, sk_2){
     let sk3 = sk_1 + sk_2;
  if (sk3 <= 100) {
     return 'True';
- }else (sk3 > 100)
- {return 'False';
  }
+ return 'False';
+ 
 }
  
 console.log(tikrinimas(22,15));
@@ -263,6 +263,7 @@ console.log(tikrinimas(3,77));
 console.log('-----------------------------------------');
 
 //6 UŽDAVINYS - sukurti funkciją, kuri nustatytų ar du paduodami argumentai yra lygūs. Grąžiname true arba false.
+// else (x1 !== x2) - šios sąlygos galima neberašyti, nes automatiškai ji prisitaiko, nes if (x1 === x2) sąlyga užfiksuota.
 
 function palyginimas (x1, x2){
 if (x1 === x2){
@@ -289,32 +290,142 @@ console.log(gyvuliai(2, 1, 1));
 
 console.log('-----------------------------------------');
 
-//8 UŽDAVINYS - sukurti funkciją, kuri nustatytų, kuris iš paduodamų parametrų (n1, n2) yra didesnis. Jei n1 didesnis grąžintų tekstą "n1 skaičius didesnis", jei n2 "n2 skaičius didesnis". Jei skaičiai lygūs grąžintų "n1 lygus n2".
+//8 UŽDAVINYS - sukurti funkciją, kuri nustatytų, kuris iš paduodamų parametrų (n1, n2) yra didesnis. Jei n1 didesnis grąžintų tekstą "n1 skaičius didesnis", jei n2 "n2 skaičius didesnis". Jei skaičiai lygūs grąžintų "n1 lygus n2". [else if(n1 == n2)] - šis tikrinimas nebereikalingas, nes arba <, arba >, kitas galimas variantas akivaizdžiai =. 
 
 function compare (n1, n2){
 if(n1 > n2){
     return `${n1} skaičius didesnis `;
 }else if(n1 < n2){
     return `${n2} skaičius didesnis `;
-}else if(n1 == n2){
-    return `${n1} lygus ${n2} `;
 }
+    return `${n1} lygus ${n2} `;
 }
 
 console.log(compare(3, 2));
 console.log(compare(2, 7));
-console.log(compare(8, 8));
+console.log(compare(9, 9));
+
+console.log('-----------------------------------------');
+
+//07.12.2022 
+console.log('-----------------------------------------');
+console.log('-----------------------------------------');
+
+//1 UŽDAVINYS - Sukurti funkciją, kuri gautų du parametrus (a,b). Funkcija grąžina 10 jei vienas iš skaičių = 10 arba tų skaičių suma = 10.
+
+function numSum (a, b){
+    if(a + b === 10){
+        return 10;
+    }else if(a === 10){
+        return 10
+    }else if(b === 10){
+        return 10
+    }
+}
+console.log(numSum(10, 9));
+console.log(numSum(2, 10));
+console.log(numSum(2, 8));
+
+//2 UŽDAVINYS - Sukurti funkciją, kuri gauna vieną parametrą skaičių (n) ir jei skaičius dalinasi iš 100, grąžina TRUE, priešinfu atveju FALSE.
+
+function part(n){
+    if(n % 100 == 0){
+        return `$(n) is even`;
+    }
+    return `$(n) is odd`;
+}
+
+console.log(part(100));
+console.log(part(1));
+console.log(part(1000));
+
+//3 UŽDAVINYS - Sukurk funkciją, kuri patikrintų ar asmuo turi leidimą ir ar asmuo yra vyresnis nei 18 metų. Abi sąlygos būtinos, kad grąžintų TRUE, priešingu atveju grąžina FALSE.
+
+function inspect (_age_, boo){
+if(_age_ === 18 && boo === true){
+    return 'true';
+}
+return 'false'
+}
+
+console.log(inspect(12, false));
+console.log(inspect(23, true));
+console.log(inspect(18, true));
+
+console.log('-----------------------------------------');
+
+//4 UŽDAVINYS - Sukurti funkciją, kuri gauna du parametrus (n, m) skaičius kaip stringą pvz "2" ir "5". Juos susumuoja ir grąžina taip pat stringą.
+
+
+function _num_(n, m){
+sum = +n + +m;
+// sum = sum.toString(); Sutrumpinimas  kur Return
+return sum.toString();
+}
+console.log(typeof _num_('2', '9'));
+console.log(_num_('', ''));
+console.log(_num_('2', ''));
+console.log(_num_('-5', '3'));
+
+console.log('-----------------------------------------');
+
+//5 UŽDAVINYS - Sukurti funkciją, kuri gauna vieną parametrą skaičių (n) ir grąžina neigiamą skaičių, jei skaičius neigiamas nieko nedaro, grąžina tą patį neigiamą skaičių.
+
+
+function return_ (n){
+    if(n >0 ){n = -n};
+    {
+        return n;
+    }
+}
+console.log(return_(-2));
+console.log(return_(7));
+console.log(return_(-15));
+
+//6 UŽDAVINYS - Kavos puodelių skaičiuotuvas. 6 nusiperki, 7 nemokamas. Sukurti funkciją, kuri apskaičiuotų kiek kavos puodelių iš viso pirkėjas gaus.
+
+//1-as variantas
+
+function cups(n){
+ let x = Math.floor(n / 6);
+  if(n >= 2){
+ return (x + n);
+}
+else if (x = 1){
+    return ++n;
+}
+}
+console.log(cups(7));
+console.log(cups(13));
+console.log(cups(18));
+console.log(cups(6));
+
+console.log('-----------------------------------------');
+
+// 2-as variantas
+
+let kv1 = 6;
+    console.log(Math.floor(kv1 / 6)+kv1); //Ats.: 7
+ let kv2 = 7;
+    console.log(Math.floor(kv2 / 6)+kv2); // Ats.:8
 
 console.log('-----------------------------------------');
 
 
 
+//7 UŽDAVINYS - Parašyti funkciją, kuri tikrintų ar paduodamo string'o ilgis yra lyginis skaičius ar ne. Grąžintų atsakymą tekstu lyginis / nelyginis.
 
+function text_ (word){
+    if (word.length % 2 == 0){
+    return `${word} yra lyginis`;
+   }
+  return `${word} yra nelyginis`;
+}
+console.log(text_('figura'));
+console.log(text_('batas'));
+console.log(text_('figura '));
 
-
-
-
-
+// console.log('-----------------------------------------');
 
 
 
